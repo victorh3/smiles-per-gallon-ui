29c0e2416fd2f504bf2b65a17408e97d12f5bb7e
victorh3 <dvictorhu@gmail.com>
2024-12-24 15:33:05 -0600

    Update Home component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,46 +1,10 @@
-import React, { useState } from "react";
-import { TextField, Button, Typography } from "@mui/material";
+import React from "react";
+import LogEntry from "./LogEntry";
 
 const Home: React.FC = () => {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-
-  const handleSignOn = () => {
-    // Handle sign-on logic here
-    console.log("Username:", username);
-    console.log("Password:", password);
-  };
-
   return (
     <>
-      <Typography variant="h4" gutterBottom>
-        Smiles Per Gallon
-      </Typography>
-      <TextField
-        label="Username"
-        variant="outlined"
-        fullWidth
-        margin="normal"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-      />
-      <TextField
-        label="Password"
-        type="password"
-        variant="outlined"
-        fullWidth
-        margin="normal"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <Button
-        variant="contained"
-        color="primary"
-        fullWidth
-        onClick={handleSignOn}
-      >
-        Sign On
-      </Button>
+      <LogEntry />
     </>
   );
 };
